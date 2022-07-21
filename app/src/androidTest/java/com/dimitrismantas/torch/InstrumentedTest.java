f66f1d6a46e68d868bd29a79ced902df9ac33690
Dimitris Mantas <dimitris.mantas@outlook.com>
2022-07-22 00:19:18 +0300

    Sign off on v1.1.0
    - Updated the project dependencies.
    - Added the required tools for the compilation of FlatBuffers schema
    files.
    - Cleaned up the test classes.
    - Added *empty* README files.
    - Cleaned up the .idea folder.
    - Reformatted and rearranged code.
    - Optimized imports.
    - Updated copyright.

--- app/src/androidTest/java/com/dimitrismantas/torch/InstrumentedTest.java
+++ app/src/androidTest/java/com/dimitrismantas/torch/InstrumentedTest.java
@@ -1,19 +1,19 @@
 /*
- * Torch is an Android application for the optimal routing of offline
- * mobile devices.
+ * Torch is a model open-source Android application for optimal routing
+ * in offline mobile devices.
  * Copyright (C) 2021-2022  DIMITRIS(.)MANTAS(@outlook.com)
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.dimitrismantas.torch;
@@ -39,7 +39,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.OptionalDouble;
 import java.util.Random;
 
 @RunWith(AndroidJUnit4.class)
@@ -49,16 +48,18 @@ public class InstrumentedTest {
     private static final int NUM_TRIALS = 3;
     private static final int NUM_WARMUP_TRIALS = NUM_AVERAGED_TRIALS / 10;
     private static final int NUM_VERTICES = 8436216;
-    private static final double TO_MILLI = 1e-6d;
+    private static final double TO_MILLI = 1E-6D;
 
     private static void warmupDevice() {
         ThreadManager.instantiateRoutingServices(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        // This makes helps make sure that the routing services are actually available before proceeding.
         try {
             Thread.sleep(5000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        final SupplementalMath math = new SupplementalMath();
+        // This class needs ot be instantiated in order for the RNG to work properly.
+        final TestMath math = new TestMath();
         for (int i = 0; i < NUM_WARMUP_TRIALS; i++) {
             final DeserializedVertex source = DataManager.getGraph().vertices(math.random(0, NUM_VERTICES));
             final DeserializedVertex target = DataManager.getGraph().vertices(math.random(0, NUM_VERTICES));
@@ -72,32 +73,21 @@ public class InstrumentedTest {
         }
     }
 
-    private static void writeToFile(final List<Double> list, final String pathname) {
-        try (final FileWriter writer = new FileWriter(FileManager.getPrimaryStorageDevicePath() + pathname)) {
-            for (final double measurement : list) {
-                writer.write(measurement + System.lineSeparator());
-            }
-        } catch (IOException e) {
-            Log.w(LOG_TAG, e);
-        }
-    }
-
-    private static void measureSingleOptimizationModeApplicationPerformance(final AStar.OptimizationMode optimizationMode, final String tag) {
+    private static void measureRoutingPerformanceForSingleOptimizationMode(final AStar.OptimizationMode optimizationMode, final String tag) {
         warmupDevice();
         Log.d(LOG_TAG, "Warmup completed.");
         // Instantiate the averaged data lists.
         final List<Double> avgR = new ArrayList<>(NUM_AVERAGED_TRIALS);
-        final List<Double> avgN = new ArrayList<>(NUM_AVERAGED_TRIALS);
         final List<Double> avgE = new ArrayList<>(NUM_AVERAGED_TRIALS);
         final List<Double> avgL = new ArrayList<>(NUM_AVERAGED_TRIALS);
         final List<Double> avgT = new ArrayList<>(NUM_AVERAGED_TRIALS);
-        final SupplementalMath math = new SupplementalMath();
+        final TestMath math = new TestMath();
         for (int i = 0; i < NUM_AVERAGED_TRIALS; i++) {
             final DeserializedVertex source = DataManager.getGraph().vertices(math.random(0, NUM_VERTICES));
             final DeserializedVertex target = DataManager.getGraph().vertices(math.random(0, NUM_VERTICES));
             // Run each trial multiple times to eliminate possible instabilities.
             final List<Integer> R = new ArrayList<>(NUM_TRIALS);
-            final List<Integer> N = new ArrayList<>(NUM_TRIALS);
+
             final List<Integer> E = new ArrayList<>(NUM_TRIALS);
             final List<Integer> L = new ArrayList<>(NUM_TRIALS);
             final List<Integer> T = new ArrayList<>(NUM_TRIALS);
@@ -109,7 +99,7 @@ public class InstrumentedTest {
                     final double t0 = System.nanoTime() * TO_MILLI;
                     final Path route = DataManager.getaStar().run(source, target, optimizationMode);
                     R.add((int) Math.round((System.nanoTime() * TO_MILLI - t0)));
-                    N.add(route.getN());
+
                     E.add(route.getEndpoints().size());
                     L.add(route.getLength());
                     T.add(route.getTravelTime());
@@ -121,31 +111,30 @@ public class InstrumentedTest {
                 }
             }
             avgR.add(math.mean(R));
-            avgN.add(math.mean(N));
+
             avgE.add(math.mean(E));
             avgL.add(math.mean(L));
             avgT.add(math.mean(T));
             Log.d(LOG_TAG, String.format("Trials Completed: %d", i + 1));
         }
-        writeToFile(avgR, tag + "R.txt");
-        writeToFile(avgN, tag + "N.txt");
-        writeToFile(avgE, tag + "E.txt");
-        writeToFile(avgL, tag + "L.txt");
-        writeToFile(avgT, tag + "T.txt");
+        TestFileManager.writeToFile(avgR, tag + "R.txt");
+        TestFileManager.writeToFile(avgE, tag + "E.txt");
+        TestFileManager.writeToFile(avgL, tag + "L.txt");
+        TestFileManager.writeToFile(avgT, tag + "T.txt");
     }
 
     @Test
-    public void measureApplicationPerformance() {
-        measureSingleOptimizationModeApplicationPerformance(AStar.OptimizationMode.MINIMIZE_DISTANCE, "A-L-");
+    public void measureRoutingPerformance() {
+        measureRoutingPerformanceForSingleOptimizationMode(AStar.OptimizationMode.MINIMIZE_DISTANCE, "2-");
         Log.d(LOG_TAG, "Torch profiled using the shortest route optimization mode.");
-        measureSingleOptimizationModeApplicationPerformance(AStar.OptimizationMode.MINIMIZE_TRAVEL_TIME, "A-T-");
+        measureRoutingPerformanceForSingleOptimizationMode(AStar.OptimizationMode.MINIMIZE_TRAVEL_TIME, "3-");
         Log.d(LOG_TAG, "Torch profiled using the fastest route optimization mode.");
     }
 
-    private static final class SupplementalMath {
+    private static final class TestMath {
         private final Random randomNumberGenerator;
 
-        private SupplementalMath() {
+        private TestMath() {
             this.randomNumberGenerator = new Random(362647020392L);
         }
 
@@ -154,11 +143,18 @@ public class InstrumentedTest {
         }
 
         private <T> double mean(final List<T> numbers) {
-            final OptionalDouble mean = numbers.stream().mapToDouble(a -> Double.parseDouble(String.valueOf(a))).average();
-            if (mean.isPresent()) {
-                return mean.getAsDouble();
-            } else {
-                return Double.NaN;
+            return numbers.stream().mapToDouble(a -> Double.parseDouble(String.valueOf(a))).average().orElse(Double.NaN);
+        }
+    }
+
+    private static final class TestFileManager {
+        private static void writeToFile(final List<Double> list, final String filename) {
+            try (final FileWriter writer = new FileWriter(FileManager.getPrimaryStorageDevicePath() + filename)) {
+                for (final double element : list) {
+                    writer.write(element + System.lineSeparator());
+                }
+            } catch (IOException e) {
+                Log.w(LOG_TAG, e);
             }
         }
     }
--- app/src/test/java/com/dimitrismantas/torch/UnitTests.java
+++ app/src/test/java/com/dimitrismantas/torch/UnitTests.java
@@ -1,35 +1,61 @@
 /*
- * Torch is an Android application for the optimal routing of offline
- * mobile devices.
+ * Torch is a model open-source Android application for optimal routing
+ * in offline mobile devices.
  * Copyright (C) 2021-2022  DIMITRIS(.)MANTAS(@outlook.com)
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.dimitrismantas.torch;
 
-import static org.junit.Assert.assertEquals;
+import com.dimitrismantas.torch.core.main.Path;
+import com.dimitrismantas.torch.core.main.engine.AStar;
+import com.dimitrismantas.torch.core.main.utils.NearestNeighborSearch;
+import com.dimitrismantas.torch.core.utils.serialization.DeserializedGraph;
+import com.dimitrismantas.torch.core.utils.serialization.DeserializedVertex;
 
 import org.junit.Test;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class UnitTests {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void routingTest() {
+        final File serializedGraphPath = new File("C:\\Documents\\Torch\\app\\src\\main\\assets\\bin\\grc.bin");
+
+        MappedByteBuffer bb = null;
+        try (final FileChannel fChannel = new RandomAccessFile(serializedGraphPath, "rw").getChannel()) {
+            bb = fChannel.map(FileChannel.MapMode.PRIVATE, 0, fChannel.size());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        final DeserializedGraph graph = DeserializedGraph.getRootAsDeserializedGraph(bb);
+
+        final double[] origin = {37.9838, 23.7275};
+        final double[] destination = {40.6401, 22.9444};
+
+        final NearestNeighborSearch nns = new NearestNeighborSearch(graph);
+        final DeserializedVertex s = nns.run(origin[0], origin[1]);
+        final DeserializedVertex t = nns.run(destination[0], destination[1]);
+
+        final AStar aStar = new AStar(graph);
+        final long t0 = System.nanoTime();
+        final Path route = aStar.run(s, t, AStar.OptimizationMode.MINIMIZE_DISTANCE);
+        System.out.println((System.nanoTime() - t0) * 1e-6);
+        System.out.println(route.getEndpoints().size());
     }
 }
