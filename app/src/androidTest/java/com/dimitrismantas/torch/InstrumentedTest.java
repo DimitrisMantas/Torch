f14900adcbe7fc617441516b16791c6d9b71f72e
Dimitris Mantas <dimitris.mantas@outlook.com>
2022-07-22 01:33:26 +0300

    Update copyright

--- app/src/androidTest/java/com/dimitrismantas/torch/InstrumentedTest.java
+++ app/src/androidTest/java/com/dimitrismantas/torch/InstrumentedTest.java
@@ -1,19 +1,19 @@
 /*
- * Torch is a model open-source Android application for optimal routing
+ * Torch is a model, open-source Android application for optimal routing
  * in offline mobile devices.
  * Copyright (C) 2021-2022  DIMITRIS(.)MANTAS(@outlook.com)
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.dimitrismantas.torch;
